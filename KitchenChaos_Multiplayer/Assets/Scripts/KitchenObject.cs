8533a3924e0b4d3ed7beb3844b2769e683082ccd
Halit Bedii Taner <bediitaner@outlook.com>
2024-05-13 00:41:47 +0300

    #4 Sync Plates Counter. Sync Kitchen Object Parent.

--- KitchenChaos_Multiplayer/Assets/Scripts/KitchenObject.cs
+++ KitchenChaos_Multiplayer/Assets/Scripts/KitchenObject.cs
@@ -1,67 +1,91 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.Netcode;
 using UnityEngine;
 
-public class KitchenObject : MonoBehaviour {
-
-
+public class KitchenObject : NetworkBehaviour
+{
     [SerializeField] private KitchenObjectSO kitchenObjectSO;
 
 
     private IKitchenObjectParent kitchenObjectParent;
+    private FollowTransform followTransform;
 
+    protected virtual void Awake()
+    {
+        followTransform = GetComponent<FollowTransform>();
+    }
 
-    public KitchenObjectSO GetKitchenObjectSO() {
+    public KitchenObjectSO GetKitchenObjectSO()
+    {
         return kitchenObjectSO;
     }
 
-    public void SetKitchenObjectParent(IKitchenObjectParent kitchenObjectParent) {
-        if (this.kitchenObjectParent != null) {
+    public void SetKitchenObjectParent(IKitchenObjectParent kitchenObjectParent)
+    {
+        SetKitchenObjectParentServerRpc(kitchenObjectParent.GetNetworkObject());
+    }
+
+    [ServerRpc(RequireOwnership = false)]
+    private void SetKitchenObjectParentServerRpc(NetworkObjectReference kitchenObjectParentNetworkObjectReference)
+    {
+       SetKitchenObjectParentClientRpc(kitchenObjectParentNetworkObjectReference);
+    }
+    
+    [ClientRpc]
+    private void SetKitchenObjectParentClientRpc(NetworkObjectReference kitchenObjectParentNetworkObjectReference)
+    {
+        kitchenObjectParentNetworkObjectReference.TryGet(out NetworkObject kitchenObjectParentNetworkObject);
+        IKitchenObjectParent kitchenObjectParent = kitchenObjectParentNetworkObject.GetComponent<IKitchenObjectParent>();
+        
+        if (this.kitchenObjectParent != null)
+        {
             this.kitchenObjectParent.ClearKitchenObject();
         }
 
         this.kitchenObjectParent = kitchenObjectParent;
 
-        if (kitchenObjectParent.HasKitchenObject()) {
+        if (kitchenObjectParent.HasKitchenObject())
+        {
             Debug.LogError("IKitchenObjectParent already has a KitchenObject!");
         }
 
         kitchenObjectParent.SetKitchenObject(this);
 
-        transform.parent = kitchenObjectParent.GetKitchenObjectFollowTransform();
+        followTransform.SetTargetTransform(kitchenObjectParent.GetKitchenObjectFollowTransform());
         transform.localPosition = Vector3.zero;
     }
-
-    public IKitchenObjectParent GetKitchenObjectParent() {
+    
+    public IKitchenObjectParent GetKitchenObjectParent()
+    {
         return kitchenObjectParent;
     }
 
-    public void DestroySelf() {
+    public void DestroySelf()
+    {
         kitchenObjectParent.ClearKitchenObject();
 
         Destroy(gameObject);
     }
 
-    public bool TryGetPlate(out PlateKitchenObject plateKitchenObject) {
-        if (this is PlateKitchenObject) {
+    public bool TryGetPlate(out PlateKitchenObject plateKitchenObject)
+    {
+        if (this is PlateKitchenObject)
+        {
             plateKitchenObject = this as PlateKitchenObject;
             return true;
-        } else {
+        }
+        else
+        {
             plateKitchenObject = null;
             return false;
         }
     }
 
 
-
-    public static KitchenObject SpawnKitchenObject(KitchenObjectSO kitchenObjectSO, IKitchenObjectParent kitchenObjectParent) {
-        Transform kitchenObjectTransform = Instantiate(kitchenObjectSO.prefab);
-
-        KitchenObject kitchenObject = kitchenObjectTransform.GetComponent<KitchenObject>();
-        
-        kitchenObject.SetKitchenObjectParent(kitchenObjectParent);
-
-        return kitchenObject;
+    public static void SpawnKitchenObject(KitchenObjectSO kitchenObjectSO, IKitchenObjectParent kitchenObjectParent)
+    {
+        KitchenGameMultiplayer.Instance.SpawnKitchenObject(kitchenObjectSO, kitchenObjectParent);
     }
-
 }
---
+++ KitchenChaos_Multiplayer/Assets/Scripts/ScriptableObjects/KitchenObjectListSO.cs
@@ -0,0 +1,10 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Unity.Multiplayer.Samples.Utilities.ClientAuthority
+{
+    public class KitchenObjectListSO: ScriptableObject
+    {
+        public List<KitchenObjectSO> kitchenObjectSOList;
+    }
+}
